1a3ba962eb44619d9df4b4376893bd55970b79b5
Omeed Safi <omeed@safi.ms>
2022-05-22 22:16:29 -0500

    Use servlet type in SpaWebMvcIntegrationTest

--- spring-boot-starter-spa/src/test/kotlin/ms/safi/spring/spa/servlet/SpaWebMvcIntegrationTest.kt
+++ spring-boot-starter-spa/src/test/kotlin/ms/safi/spring/spa/servlet/SpaWebMvcIntegrationTest.kt
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest.WebEnvironment
 @SpringBootTest(
     webEnvironment = WebEnvironment.RANDOM_PORT,
     properties = [
-        "spring.main.web-application-type=reactive"
+        "spring.main.web-application-type=servlet"
     ]
 )
 internal class SpaWebMvcIntegrationTest : SpaIntegrationTest()
